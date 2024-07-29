a55a91c013aaeb6e38542f89ae34aa6cf6af625b
LucifeR278 <ashishchamp323@gmail.com>
2024-07-29 10:45:03 +0530

    dkjdn

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain_community.vectorstores import FAISS
-
+from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -105,8 +104,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
